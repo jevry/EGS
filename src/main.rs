30da6d099230f78e62b4bb0f693a0090d57bdba7
jevry <kieranvangelder01@gmail.com>
2024-04-23 16:07:14 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@
 
 use std::{env::Vars, fmt::Debug};
 
-use crate::test::TreeNode;
-use daggy::{self, petgraph::{graph::{node_index, Node}, visit::{GraphRef, IntoNeighborsDirected, IntoNodeReferences}, Graph}, walker::{self, Chain, Inspect, TakeWhile}, Children, Dag, NodeIndex, Walker};
+
+use daggy::{self, petgraph::{graph::{node_index, Node},
+    visit::{GraphRef, IntoNeighborsDirected, IntoNodeIdentifiers, IntoNodeReferences}, Graph}, stable_dag::{self, StableDag}, walker::{self, Chain, Inspect, TakeWhile}, Children, NodeIndex, Walker
+};
 
 
 pub(crate) type BuildHasher = fxhash::FxBuildHasher;
 pub(crate) type IndexMap<K, V> = indexmap::IndexMap<K, V, BuildHasher>;
 pub(crate) type IndexSet<K> = indexmap::IndexSet<K, BuildHasher>;
 
-mod test;
+
 
 #[macro_export]
 macro_rules! mstr { //simplify string::from function
@@ -20,87 +22,44 @@ macro_rules! mstr { //simplify string::from function
     };
 }
 
-struct Globals {
-    boolean: bool,
-    first_string: String,
-    second_string: String,
-}
-
-// workaround to get as close as rust lets you get to globals
-fn set_globals() -> Globals {
-    Globals {
-        boolean: true,
-        first_string: mstr!("I am"),
-        second_string: mstr!(" a Global!"),
-    }
-}
-
-fn return_true() -> bool {
-    true
-}
 
 
-
-fn print_some_glob_stuff(mut g: Globals){
-    g.first_string.push_str(" definitely");
-    g.boolean = false;
-    println!("{} {}", g.first_string, g.second_string);
-}
-
-
-fn print_dag(mut g: Dag<u32, u32>){
+fn print_dag(g: StableDag<u32, u32>, start_idx: NodeIndex){
     if (g.node_count()== 0) {return;}
-
-    for n in g.raw_nodes() {
-        //runs once for every node in the network
-    }
-    let temp = g.raw_nodes();
-    let w1 = g.children(node_index(0));
-
-
-    
-
-    let mut expensive_closure =
-                |arg0: &Dag<u32, u32>, arg1: &(daggy::EdgeIndex, NodeIndex)| -> bool {
+    print!("size = {:?}\n", g.node_count());
+    let mut simple_fn =
+                |mut arg0: &StableDag<u32, u32>,
+                mut arg1: &(daggy::EdgeIndex, NodeIndex)| -> bool {
         let a = true;
+        print!("    child: {:?}\n", arg1.1);
         return a;
     };
 
-
-
-    let w2 =
-        TakeWhile::new(w1, expensive_closure);
-
-    for (e, n) in w2.iter(&g){
-        print!("we be walkin nodes {:?} {:?}\n", e, n);
+    for n_id in g.node_identifiers() {
+        print!("node {:?}\n", n_id);
+        let w1 = g.children(n_id);
+        let w2 =
+            walker::TakeWhile::new(w1, simple_fn);
+        for (e, n) in w2.iter(&g){
+            //print!("we be walkin nodes {:?} {:?}\n", e, n);
+        }
+        print!("\n");
     }
 }
 
 
 fn main() {
-    // let g = set_globals();
-    // let mut this_var_is = "useless";
 
-    // let mut s = String::from("hello");
-    // s.push_str(", world!");
-    // let int = 5;
-    // if int == 5 {
-    //     println!("{}", s);
-    // }
-    // let testnode = test::build_node(mstr!("I am a node!"));
-    // println!("{}", testnode.strn);
-
-    // print_some_glob_stuff(g);
-    
-    ///TAG testing
-
-    print!("-----\n");
-
-    let mut graph: Dag<u32, u32, u32> = daggy::Dag::new();
+    let mut graph: StableDag<u32, u32, u32>= stable_dag::StableDag::new();
     let idx = graph.add_node(1);
+
     graph.add_child(idx, 1, 1);
-    graph.add_child(idx, 1, 1);
-    print_dag(graph);
+    let (_, idx2) = graph.add_child(idx, 1, 1);
+    let (_, idx2) = graph.add_child(idx2, 1, 1);
+    graph.add_child(idx2, 1, 1);
+    graph.add_child(idx2, 1, 1);
+
+    print_dag(graph, idx);
 
     
 
